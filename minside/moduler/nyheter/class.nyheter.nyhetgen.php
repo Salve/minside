6fb479ee2755a030e84f5469f912f96820eaac28
Salve Spinnangr <salve.spinnangr@gmail.com>
2010-09-02 09:30:54 +0200

    Flyttet toolbar ut av form + fikset bug i IE
    Signed-off-by: Salve Spinnangr <salve.spinnangr@gmail.com>

--- minside/moduler/nyheter/class.nyheter.nyhetgen.php
+++ minside/moduler/nyheter/class.nyheter.nyhetgen.php
@@ -3,6 +3,8 @@ if(!defined('MS_INC')) die();
 
 class NyhetGen {
 
+	const THUMB_BREDDE = 100;
+
 	private function __construct() { }
 	
 	public static function genFullNyhetViewOnly(msnyhet &$nyhet) {
@@ -21,9 +23,9 @@ class NyhetGen {
 		$type = $nyhet->getType();		
 		$id = $nyhet->getId();
 		if ($nyhet->hasImage()) {
-			$imgpath = $nyhet->getImagePath();
+			$img = $nyhet->getImageTag(self::THUMB_BREDDE);
 		} else {
-			$imgpath = false;
+			$img = '';
 		}
 		$title = $nyhet->getTitle();
 		$body = $nyhet->getHtmlBody();
@@ -61,7 +63,7 @@ class NyhetGen {
 		return $output;
 		
 	}
-    
+
     public static function genEdit(msnyhet &$objNyhet) {
 		
 		// Område
@@ -120,20 +122,7 @@ class NyhetGen {
 				
         $output .= '<div class="editnyhet">';
         $output .= '<p><strong>Rediger nyhet</strong></p>';
-        $output .= '<form action="' . MS_NYHET_LINK . '&act=subedit" method="POST">';
-        $output .= '<input type="hidden" name="nyhetid" value="' . $objNyhet->getId() . '" />';
-        $output .= 'Overskrift: ';
-		$output .= '<input type="text" name="nyhettitle" value="' . $objNyhet->getTitle() . '" /><br />';
-		$output .= $html_omrade . "<br />\n";
-		$output .= $html_calendar . "<br />\n";
-		$output .= $html_viktighet . "<br />\n";
-        
-        $rawwiki = formText(rawWiki($objNyhet->getWikiPath()));
-        
-        $output .= '
-			
-            <div style="width:99%;">
-            <div class="toolbar">
+		$output .= '<div class="toolbar">
                 <div id="draft__status"></div>
                 <div id="tool__bar"><a href="/wiki/lib/exe/mediamanager.php?ns=msnyheter"
                     target="_blank">Valg av mediafil</a></div>
@@ -141,15 +130,29 @@ class NyhetGen {
                 <script type="text/javascript" charset="utf-8"><!--//--><![CDATA[//><!--
                     textChanged = false;
                     //--><!]]></script>
-             </div>
-             
+             </div>';
+        $output .= '<form action="' . MS_NYHET_LINK . '&act=subedit" method="POST">';
+        
+		$output .= '<input type="hidden" name="nyhetid" value="' . $objNyhet->getId() . '" />';
+        
+        $rawwiki = formText(rawWiki($objNyhet->getWikiPath()));
+        
+        $output .= '
+			
+            <div style="width:99%;">
+                         
             
             <input type="hidden" name="id" value="'.$objNyhet->getWikiPath().'" />
             <input type="hidden" name="rev" value="" />
             <textarea name="wikitext" id="wiki__text" class="edit" cols="80" rows="10" tabindex="1" >'
             . $rawwiki .
-            '</textarea>
-            <div id="wiki__editbar" >
+            '</textarea>';
+			$output .= 'Overskrift: ';
+		$output .= '<input type="text" name="nyhettitle" value="' . $objNyhet->getTitle() . '" /><br />';
+		$output .= $html_omrade . "<br />\n";
+		$output .= $html_calendar . "<br />\n";
+		$output .= $html_viktighet . "<br />\n";
+            $output .= '<div id="wiki__editbar" >
             <div id="size__ctl" >
             </div>
             <div class="editButtons" >
@@ -165,6 +168,7 @@ class NyhetGen {
             </form>
         ';
         $output .= '</div>'; // editnyhet
+		
         return $output;
     }
 	
