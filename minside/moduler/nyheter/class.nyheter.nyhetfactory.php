77fa15dcee60dda55ca8afb2ea6066585362e6c1
Salve Spinnangr <salve.spinnangr@gmail.com>
2010-11-16 16:18:27 +0100

    Bugfix, overskriftsøk håndterer ikke "?" som single char wildcard
    Replacer med underscore som mysql bruker for dette.
    
    Signed-off-by: Salve Spinnangr <salve.spinnangr@gmail.com>

--- minside/moduler/nyheter/class.nyheter.nyhetfactory.php
+++ minside/moduler/nyheter/class.nyheter.nyhetfactory.php
@@ -274,7 +274,7 @@ class NyhetFactory {
         // Tildato
         if (array_key_exists('tdato', $limits)) $where[] = "pubtime <= '" . date('Y-m-d', $limits['tdato']) . ' 23:59:59' . "'";
         // Overskriftsøk
-        if (array_key_exists('oskrift', $limits)) $where[] = "nyhettitle LIKE " . $msdb->quote(str_replace('*', '%', $limits['oskrift']));
+        if (array_key_exists('oskrift', $limits)) $where[] = "nyhettitle LIKE " . $msdb->quote(str_replace(array('*', '?'), array('%', '_'), $limits['oskrift']));
         // Sortorder
         $safesortorder = ($limits['sortorder']) ?: 'DESC';
         // Limit
