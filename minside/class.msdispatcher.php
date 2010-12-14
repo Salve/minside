796f6acc433de5ae5ffe19bbbc9d86684481601f
Njål Kollbotn <njalk@igrudge.net>
2010-12-14 11:04:16 +0100

    Lagt til 'stille' variabel i visning av moduloutput.
    Signed-off-by: Njål Kollbotn <njalk@igrudge.net>

--- minside/class.msdispatcher.php
+++ minside/class.msdispatcher.php
@@ -20,8 +20,8 @@ class msdispatcher {
 		if (array_key_exists("$this->ms",$this->msmoduler)){
 			if ($this->adgang->sjekkAdgang($this->ms) > 0) {
 				return $this->msmoduler["$this->ms"]->gen_msmodul($this->act, $this->vars);
-			} else {
-				throw new AdgangsException('Dispatcher: Innlogget bruker har ikke adgang til å vise modulen "' . $this->ms . '".');
+			} else {// Sjekker om 'stille' variabel er satt i modulvisning, og viser ingen output dersom denne er satt, viser feilmelding dersom den ikke er satt.
+			    if (!array_search('stille', $this->vars, true)) throw new AdgangsException('Dispatcher: Innlogget bruker har ikke adgang til å vise modulen "' . $this->ms . '".');
 			}
 		} else {
 			return '<p>Dispatcher: Klarte ikke å laste modulen "' . $this->ms . '".</p>';		
