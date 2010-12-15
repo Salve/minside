4e9fb1918fccfe76deeb76297240655346eb3655
Salve Spinnangr <salve.spinnangr@gmail.com>
2010-12-15 16:09:57 +0100

    Viser adgangsgruppe i stedet for navn i datagrunnlaget til nyhetstats
    Signed-off-by: Salve Spinnangr <salve.spinnangr@gmail.com>

--- minside/moduler/nyheter/class.nyheter.nyhetgen.php
+++ minside/moduler/nyheter/class.nyheter.nyhetgen.php
@@ -706,10 +706,10 @@ class NyhetGen {
         $section_tabell = '<h2>Datagrunnlag</h2><div class="level3">';
         $section_tabell .= 'Tab-separert data over lesetidspunkt. Kan kopieres rett inn i Excel.<br>
             Brukere med blankt lesetidspunkt har ikke markert nyhet som lest.<pre>';
-        $section_tabell .= "BrukerID\tNavn\tTidspunkt lest\n";
+        $section_tabell .= "BrukerID\tAdgangsgruppe\tTidspunkt lest\n";
         foreach($arReadList as $readevent) {
             $section_tabell .= $readevent['brukerid'] . "\t";
-            $section_tabell .= $readevent['brukerfullnavn'] . "\t";
+            $section_tabell .= $readevent['brukergrupper'] . "\t";
             $section_tabell .= $readevent['readtime'] . "\n";
         }
         $section_tabell .= '</pre></div>';
