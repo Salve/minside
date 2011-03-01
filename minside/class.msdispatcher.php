bebc65e7fffec4d9639129e9dced422c4e3e535a
Salve Spinnangr <salve.spinnangr@gmail.com>
2011-03-01 12:49:55 +0100

    Mulighet for å supresse access-error fra modul dispatcher
    Til bruk på forside, slik at brukere uten adgang til en modul som vises
    der ikke får feilmelding.
    
    Bruksmåte: legg til :stille på slutten av minside-syntax
    
    Issue #49 i github tracker.
    
    Signed-off-by: Salve Spinnangr <salve.spinnangr@gmail.com>

--- minside/class.msdispatcher.php
+++ minside/class.msdispatcher.php
@@ -10,7 +10,7 @@ class msdispatcher {
 	
 	function __construct($event_handle, &$msmoduler, &$adgang, $act = NULL, $vars = array()){
 		$this->ms = $event_handle;
-		$this->vars = $vars;
+		$this->vars = (array) $vars;
 		$this->act = $act;
 		$this->msmoduler =& $msmoduler;
 		$this->adgang =& $adgang;
@@ -20,8 +20,9 @@ class msdispatcher {
 		if (array_key_exists("$this->ms",$this->msmoduler)){
 			if ($this->adgang->sjekkAdgang($this->ms) > 0) {
 				return $this->msmoduler["$this->ms"]->gen_msmodul($this->act, $this->vars);
-			} else {// Sjekker om 'stille' variabel er satt i modulvisning, og viser ingen output dersom denne er satt, viser feilmelding dersom den ikke er satt.
-			    if (!array_search('stille', $this->vars, true)) throw new AdgangsException('Dispatcher: Innlogget bruker har ikke adgang til å vise modulen "' . $this->ms . '".');
+			} else {
+                // Sjekker om 'stille' variabel er satt i modulvisning, og viser ingen output dersom denne er satt, viser feilmelding dersom den ikke er satt.
+			    if (array_search('stille', $this->vars) === false) throw new AdgangsException('Dispatcher: Innlogget bruker har ikke adgang til å vise modulen "' . $this->ms . '".');
 			}
 		} else {
 			return '<p>Dispatcher: Klarte ikke å laste modulen "' . $this->ms . '".</p>';		
