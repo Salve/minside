d39bf39b854126bb9772a95b032888bf8fe52f63
Salve Spinnangr <salve.spinnangr@gmail.com>
2011-06-10 03:33:37 +0200

    Bed tools done
    Signed-off-by: Salve Spinnangr <salve.spinnangr@gmail.com>

--- minside/moduler/bedtools/bedtools.msmodul.php
+++ minside/moduler/bedtools/bedtools.msmodul.php
@@ -1,5 +1,7 @@
 <?php
 if(!defined('MS_INC')) die();
+define('MS_BEDTOOLS_LINK', MS_LINK . "&amp;page=bedtools");
+require_once('class.bedtools.bedtoolsgen.php');
 
 class msmodul_bedtools implements msmodul{
 
@@ -7,6 +9,8 @@ class msmodul_bedtools implements msmodul{
 	protected $_adgang;
     protected $_act;
     protected $_vars;
+    
+    protected $dispatcher;
 
 	
 	public function __construct($UserID, $adgang) {
@@ -18,8 +22,19 @@ class msmodul_bedtools implements msmodul{
 		$this->_act = $act;
         $this->_vars = $vars;
         
-        return 'test';
+        // Opprett ny dispatcher
+		$this->dispatcher = new ActDispatcher($this, $this->_adgang);
+		// Funksjon som definerer handles for act-values
+		$this->_setHandlers($this->dispatcher);
+		
+		// Dispatch $act, dispatcher returnerer output
+		return $this->dispatcher->dispatch($act);
 	}
+    
+    private function _setHandlers(&$dispatcher) {
+        $dispatcher->addActHandler('show', 'gen_forside', MSAUTH_1);
+        $dispatcher->addActHandler('genvarsling', 'gen_varsling', MSAUTH_1);
+    }
 	
 	public function registrer_meny(MenyitemCollection &$meny){
 		$lvl = $this->_adgang;
@@ -35,4 +50,91 @@ class msmodul_bedtools implements msmodul{
 			$meny->addItem($toppmeny); 
 		}
 	}
+    
+    // HANDLERS
+    
+    public function gen_forside() {
+        return BedToolsGen::genForside();
+    }
+    
+    public function gen_varsling() {
+        $type = $_POST['type_input'];
+        $data = trim($_POST['varsel_data']);
+        
+        if ($type != "KID" && $type != "TLF") {
+            throw new Exception('Ukjent handling, velg kundenummer eller telefonnummer');
+        }
+        
+        if(!strlen($data)) {
+            throw new Exception('Ingen input gitt!');
+        }
+        
+        if ($type == "KID") {
+            return $this->genKID($data);
+        } elseif ($type == "TLF") {
+            return $this->genTLF($data);
+        }
+                
+    }
+    
+    protected function genKID($inn_data) {
+        $data = explode("\n", $inn_data);
+        $ut_data = array();
+        $antall_blanke = 0;
+        msg('Input er på ' . count($data) . ' linjer.');
+        foreach($data as $datum) {
+            $kid = (string) (double) trim($datum);
+            $len = strlen($kid);
+            if($len != 8) {
+                if($datum == 0) {
+                    $antall_blanke += 1;
+                    continue;
+                }
+                // Ikke 8 siffer, men heller ikke blankt
+                msg("Ugyldig KID: " . hsc($kid) . "! Hopper over dette.", -1);
+                continue;
+            }
+            
+            // Gyldig KID
+            $ut_data[] = '\'' . $kid . '\'';
+        }
+        msg($antall_blanke . ' blanke linjer ignorert.');
+        msg('Spørring med ' . count($ut_data) . ' kundenummer generert!', 1);
+        $kid_streng = implode(', ', $ut_data);
+        $output = '"Customer Profile"."Account Nr" IN (' . $kid_streng . ')'; 
+        
+        return '<br /><br /><br />' . $output;
+    }
+    
+    protected function genTLF($inn_data) {
+        $data = explode("\n", $inn_data);
+        $ut_data = array();
+        $antall_blanke = 0;
+        msg('Input er på ' . count($data) . ' linjer.');
+        foreach($data as $datum) {
+            $tlf = (string) (double) trim($datum);
+            if(strlen($tlf) == 10) {
+                $tlf = substr($tlf, 2);
+            }
+            $forste_siffer = substr($tlf, 0, 1);
+            if(strlen($tlf) != 8 || ($forste_siffer != 4 && $forste_siffer != 9)) {
+                if($datum == 0) {
+                    $antall_blanke += 1;
+                    continue;
+                }
+                msg("Ugyldig mobilnummer: " . hsc($tlf) . "! Hopper over dette.", -1);
+                continue;
+            }
+            
+            // Gyldig TLF
+            $ut_data[] = $tlf;
+        }
+        sort($ut_data, SORT_NUMERIC);
+        msg($antall_blanke . ' blanke linjer ignorert.');
+        msg('Liste med ' . count($ut_data) . ' mobilnummer generert!', 1);
+        $tlf_streng = implode(';', $ut_data);
+        
+        return '<br /><br /><br />' . $tlf_streng;
+        
+    }
 }
---
+++ minside/moduler/bedtools/class.bedtools.bedtoolsgen.php
@@ -0,0 +1,32 @@
+<?php
+if(!defined('MS_INC')) die();
+
+class BedToolsGen {
+
+    public static function genForside() {
+        return '
+            <h1>Bedriftsverktøy</h1>
+            <div class="level1">
+                <h2>SMS-varsling</h2>
+                <div class="level2">
+                    '.self::genInputFormVarsling().'
+                </div>
+            </div>
+        ';
+    }
+    
+    protected static function genInputFormVarsling() {
+        return '
+           <form action="' . MS_BEDTOOLS_LINK . '&amp;act=genvarsling" method="POST">
+                <input type="radio" id="radio_kid" name="type_input" value="KID" />
+                <label for="radio_kid">Kundenummer til Siebel Answers</label><br />
+                <input type="radio" id="radio_tlf" name="type_input" value="TLF" />
+                <label for="radio_tlf">Telefonnummer til sms-utsendelse</label><br />
+                <textarea name="varsel_data" cols="14" rows="10"></textarea><br />
+                <br /><br /><input type="submit" value="Generer streng" class="button" />
+            </form>
+        
+        ';
+    }
+
+}
